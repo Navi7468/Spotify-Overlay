dac8920f4ec448cb5a7d50985f5e01c3d113be43
Navi <96448085+Navi7468@users.noreply.github.com>
2023-11-14 12:23:18 -0800

    Update 1

--- main.js
+++ main.js
@@ -1,74 +1,31 @@
-const { app, BrowserWindow, Menu } = require('electron');
-const path = require('path');
-const url = require('url');
-require('dotenv').config();
-const port = process.env.PORT || 1111;
-
-
-
-
-
-// Create WebSocket Server
-const socket = require("ws");
-
-var clients = [];
-var lastMessages = [];
-const webSocket = new socket.Server({ port: port });
-
-webSocket.on("listening", () => {
-    console.log("Listening on port " + port);
-});
-
-webSocket.on("connection", wsClient => {
-    console.log("connection!");
-    clients.push(wsClient);
-
-    wsClient.on("message", msg => {
-        // console.log("Message Received: " + msg);
-        clients.forEach(client => {
-            if (client.readyState === socket.OPEN) {
-                client.send(msg.toString());
-            }
-        });
-    });
-
-    wsClient.on("close", () => {
-        console.log("Client Disconnected");
-    });
-})
-
-
-
-
-
+const { app, BrowserWindow, Menu, protocol } = require('electron');
 
 
 app.on('ready', () => {
+    console.log('App is ready!');
     const win = new BrowserWindow({
-        width: 520,
-        height: 170,
-        icon: 'MainPage/lib/trex.png',
+        x: 600,
+        y: 300,
+        width: 750,
+        height: 500,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
         },
         // resizable: false,
-        transparent: true,
-        frame: false,
+        // transparent: true,
+        // frame: false,
         alwaysOnTop: true
     });
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, './index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
+
+    win.loadURL('http://localhost:2000/callback');
+    win.loadFile('src/views/index.html');
     win.on('closed', () => { app.quit() });
+
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
     Menu.setApplicationMenu(mainMenu);
 
 });
-
-
 
 
 const mainMenuTemplate = [
